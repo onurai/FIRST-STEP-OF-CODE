b4b22c38d2ebf06921361fefbda4daf7251fe881
onurai <onurai@code.edu.az>
2022-07-31 09:04:52 +0300

    first code lesson

---
+++ First project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello guys my name is Onur");
+        }
+    }
+}
